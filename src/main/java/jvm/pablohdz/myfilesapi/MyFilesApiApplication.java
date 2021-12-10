80f5d544f2f3802125b88200c5b70830c70c79fa
Pablo Hernandez <55292284+PabloHdzVizcarra@users.noreply.github.com>
2021-12-09 18:04:00 -0600

    start project

---
+++ src/main/java/jvm/pablohdz/myfilesapi/MyFilesApiApplication.java
@@ -0,0 +1,13 @@
+package jvm.pablohdz.myfilesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFilesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFilesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jvm/pablohdz/myfilesapi/MyFilesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package jvm.pablohdz.myfilesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFilesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

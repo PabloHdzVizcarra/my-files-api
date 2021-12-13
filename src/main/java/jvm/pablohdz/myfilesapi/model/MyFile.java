63a9d9cd503a9df4e8d6de178fd2fb5143214b2e
Pablo Hernandez <55292284+PabloHdzVizcarra@users.noreply.github.com>
2021-12-13 08:18:28 -0600

    new entity to represents a CSV file

---
+++ src/main/java/jvm/pablohdz/myfilesapi/model/MyFile.java
@@ -0,0 +1,114 @@
+package jvm.pablohdz.myfilesapi.model;
+
+import java.util.Date;
+import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+/**
+ * Represents a CSV file stored
+ * */
+@Entity
+@Table(name = "file")
+public class MyFile {
+  @Id
+  @Column(name = "file_id")
+  private String id;
+
+  @Column(name = "file_storage_id", nullable = false)
+  private String storageId;
+
+  @Column(name = "file_name")
+  private String name;
+
+  @Column(name = "file_version")
+  private Double version;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id")
+  private User user;
+
+  @CreatedDate
+  @Column(name = "user_create_at", nullable = false, updatable = false)
+  private Date createdAt;
+
+  @LastModifiedDate
+  @Column(name = "user_update_at")
+  private Date updateAt;
+
+  public MyFile() {
+    this.id = "fi_" + UUID.randomUUID();
+  }
+
+  @PrePersist
+  protected void prePersist() {
+    if (this.createdAt == null) createdAt = new Date();
+    if (this.updateAt == null) updateAt = new Date();
+  }
+
+  @PreUpdate
+  protected void preUpdate() {
+    this.updateAt = new Date();
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public String getStorageId() {
+    return storageId;
+  }
+
+  public void setStorageId(String url) {
+    this.storageId = url;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Double getVersion() {
+    return version;
+  }
+
+  public void setVersion(Double version) {
+    this.version = version;
+  }
+
+  public User getUser() {
+    return user;
+  }
+
+  public void setUser(User user) {
+    this.user = user;
+  }
+
+  public Date getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(Date createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public Date getUpdateAt() {
+    return updateAt;
+  }
+
+  public void setUpdateAt(Date updateAt) {
+    this.updateAt = updateAt;
+  }
+}

2e21018fb3acd89cd3e518f22e87075c2000e24f
Pablo Hernandez <55292284+PabloHdzVizcarra@users.noreply.github.com>
2021-12-15 10:03:42 -0600

    retrieve file from the AWS S3 bucket with name

--- src/main/java/jvm/pablohdz/myfilesapi/service/CSVFileStorageService.java
+++ src/main/java/jvm/pablohdz/myfilesapi/service/CSVFileStorageService.java
@@ -1,5 +1,9 @@
 package jvm.pablohdz.myfilesapi.service;
 
+import org.springframework.core.io.InputStreamResource;
+
 public interface CSVFileStorageService {
   String upload(byte[] file, String fileName);
+
+  InputStreamResource getFile(String storageId);
 }

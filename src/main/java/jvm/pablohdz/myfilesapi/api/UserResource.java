506d84bba6f2e5b0f183a87845e05162aa7049c2
Pablo Hernandez <55292284+PabloHdzVizcarra@users.noreply.github.com>
2021-12-17 09:54:29 -0600

    create new endpoint to get all files

--- src/main/java/jvm/pablohdz/myfilesapi/api/UserResource.java
+++ src/main/java/jvm/pablohdz/myfilesapi/api/UserResource.java
@@ -1,15 +1,19 @@
 package jvm.pablohdz.myfilesapi.api;
 
+import java.util.Collection;
+import java.util.List;
+import jvm.pablohdz.myfilesapi.service.CSVService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
-import java.net.UnknownHostException;
 
 import javax.validation.Valid;
 
@@ -17,21 +21,30 @@ import jvm.pablohdz.myfilesapi.dto.UserRequest;
 import jvm.pablohdz.myfilesapi.service.UserService;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api")
 public class UserResource {
   private final UserService userService;
+  private final CSVService csvService;
 
   @Autowired
-  public UserResource(UserService userService) {
+  public UserResource(UserService userService, CSVService csvService) {
     this.userService = userService;
+    this.csvService = csvService;
   }
 
   @PostMapping(
+      value = "/users",
       consumes = MediaType.APPLICATION_JSON_VALUE,
       produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<String> create(@Valid @RequestBody UserRequest userRequest) {
     userService.create(userRequest);
     URI location = URI.create("/api/users" + userRequest.getUsername());
     return ResponseEntity.created(location).build();
+  }
+
+  @GetMapping(value = "/user/{userId}/files", produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<Collection<String>> getAllFiles(@PathVariable("userId") String userId) {
+    csvService.getAllFilesByUserId(userId);
+    return ResponseEntity.ok(List.of());
   }
 }
